d255c187da8ed9489f73f22b252263bfd5b3810d
Hari8595 <112604938+Hari8595@users.noreply.github.com>
2023-05-31 09:41:44 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,10 +1,13 @@
 def buildApp () {
   echo "Building the stage"
+  echo "Version is $VERSION"
+  sh "mvn --version"
 }
 def testApp () {
   echo "Testing the stage"
 }
 def deployApp () {
   echo "Deploying the stage"
+  echo "params.TYPE"
 }
 return this
