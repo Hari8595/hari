4665713cc2b4f57b6fe64f46dcbcf37d7e766a16
Hari8595 <112604938+Hari8595@users.noreply.github.com>
2023-05-31 18:06:24 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -4,9 +4,9 @@ def buildJar() {
 }
 def image() {
     withCredentials([usernamePassword(credentialsId: "Docker", usernameVariable: "USR", passwordVariable: "PASS")]) {
-        sh "docker build -t hari851995/java:v1.4 ."
+        sh "docker build -t hari851995/java:v1.5 ."
         sh "echo $PASS | docker login -u $USR --password-stdin"
-        sh "docker push hari851995/java:v1.4" 
+        sh "docker push hari851995/java:v1.5" 
     }
 }
 def deploy() {
