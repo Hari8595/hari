cd84266bb53c99c8f10698c958f94586d8512116
Hari8595 <112604938+Hari8595@users.noreply.github.com>
2023-05-31 17:25:44 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,15 @@
 def buildJar() {
-  echo "Building the images"
-  sh "mvn package"
+    echo "Building the images"
+    sh "mvn package"
 }
 def image() {
-  withCredentials([usernamePassword(credentialsId: "Docker", usernameVariable: "USR", passwordVariable: "PASS")]) {
-  sh "docker build -t hari851995/java:v1.4 ."
-  sh "echo $PASS | docker login -u $USR --password-stdin"
-  sh "docker push hari851995/java:v1.4"
+    withCredentials([usernamePassword(credentialsId: "Docker", usernameVariable: "USR", passwordVariable: "PASS")]) {
+        sh "docker build -t hari851995/java:v1.4 ."
+        sh "echo $PASS | docker login -u $USR --password-stdin"
+        sh "docker push hari851995/java:v1.4" 
+    }
 }
 def deploy() {
-  echo "Deploying into server"
+    echo "Deploying into server"
 }
 return this
